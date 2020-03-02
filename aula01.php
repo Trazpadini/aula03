ca06c4162717e233a444108a96ad8de2b969d51d
trazpadini <luanatrazpadini@hotmail.com>
2020-03-02 09:33:03 -0300

    noticias

---
+++ aula01.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title> Jornal Luanal </title>
+</head>
+<body>
+  <style media= "screen">
+    .noticias  h1 {
+      color:  #001a1a;
+      font-size: 50px;
+    }
+    .noticias p {
+      font-size: 20px;
+    }
+    </style>
+
+  <div class="noticias">
+    <h1> Rihanna foi decretada rainha do mundo </h1>
+    <img src="https://lh3.googleusercontent.com/proxy/EnhJ3FjOPYxM3E32z3VHx_nq2u_J9xYac92HKgZu75LP5tdg1uSm2f7m6d8G4DvccIInzuW9sTLzB1QYAQELtz8pZR2L2NUDcjipicfGVog">
+    <p>
+      Foi definido hoje o auge da beleza mundial, Rihanna,
+      o que dizer da nossa rainha, bela como és,
+      perfeita do jeito que é, estupenda, inteligentíssima e em outras palavras:
+      P E R F E I T A
+    </p>
+  </div>
+
+  <div class="noticias">
+    <h1> Mirian mostrou a raba pra ele </h1>
+    <table style="width:60%">
+      <tr>
+        <th>Locais da perversão</th>
+        <th>Quantidades de crushs</th>
+      </tr>
+
+      <tr>
+        <td>ÉPOCA FOLIA</td>
+        <td>3</td>
+      </tr>
+      <tr>
+        <td>HALLOWEEN DA FEDERAL</td>
+        <td>1</td>
+      </tr>
+    </table>
+    <p>
+      Mirian, a louca das festas, conhecida como passa sapinho.
+      Falaremos então dessa PERVERTIDA, ao se embreagar, jovem promíscua passa herpes a 8 jovens.
+      Sua fama não nega, é só beija-la que a doença vem. E a vergonha também.
+    </p>
+  </div>
+
+    <div class="noticias">
+      <h1> PABLO VITTAR FOI LONGE DEMAIS </h1>
+      <img src="https://s2.glbimg.com/_9fHZnXv1N7B6zZOxI0yEZo9pl4=/0x0:1200x750/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2020/j/N/AZiUwwQlAtM41fX3pzwA/pabllorajadao.jpg">
+      <p>
+        A cantora Pablo Vittar anunciou nessa segunda-feira 02 de março que vai para Marte. YUKÊÊÊ???
+        Fãs ficam chocados com a notícia, porém a cantora tranquiliza.
+        Diz Pablo que fará transmissão ao vivo todos os dias.
+      </p>
+      <p>
+        O primeiro show em Marte tem dia marcado e a apresentação será de uma DRAG QUEEN!!!! É para poucos esse poder.
+        Marque no seu calendário que dia 9 de dezembro pois terá muito YUKÊÊ e muito Amor de Kenga com Pablo em Marte madameeees!
+      </p>
+    </div>
